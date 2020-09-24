d046ba34ad62e688617ee0292129170e46013ee4
Monica Djunaidi <modjun12@gmail.com>
2020-09-24 18:13:35 -0500

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,11 @@
-test('Fake Test', () => {
+import { add } from './App';
+
+//unit test
+//only tests one thing
+test('add', () => {
   //assertion
-  expect(true).toBeTruthy();
+  // const value = add(1,2);
+  // expect(value).toBe(3);
+  expect(add(1,2)).toBe(3);
+  expect(add(5,2)).toBe(7);
 })
