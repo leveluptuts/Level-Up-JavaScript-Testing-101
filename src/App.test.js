02bce5d0f270ae8b528d775ebd70c22c2e0f0ce2
Monica Djunaidi <modjun12@gmail.com>
2020-09-24 18:33:41 -0500

    added mock test

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,22 @@
-import { add, total } from './App';
+import { total } from './App';
 
-//unit test
-//only tests one thing
+//Mock test
+//creating mock function
+//test function when don't have access to function
+const add = jest.fn(() => 3);
+
+//Nnit test - only tests one thing
 test('add', () => {
   //assertion
   // const value = add(1,2);
   // expect(value).toBe(3);
   expect(add(1,2)).toBe(3);
-  expect(add(5,2)).toBe(7);
+  expect(add).toHaveBeenCalledTimes(1);
+  expect(add).toHaveBeenCalledWith(1, 2);
+  // expect(add(5,2)).toBe(7);
 })
 
-//Integration test
-//testing function based on another function
-test('total', () => {
-  expect(total(5, 20)).toBe('$25');
-})
+//Integration test - testing function based on another function
+// test('total', () => {
+//   expect(total(5, 20)).toBe('$25');
+// })
